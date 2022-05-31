c4033acf824e11cae9e9b14e617c047c6063965d
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-31 13:10:45 +0530

    [Harini]: uc-1 case - creation of Linkedlist to add values and display it

---
+++ LinkedList/LinkedList/LinkedListClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedListClass
+    
+    {
+        
+        internal Node head;
+        
+        public void add(int id)
+        {
+           
+
+            Node node = new Node(id);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            
+            Console.WriteLine(" Adding values in linked list : {0} ", node.value);
+            
+        }
+        public void display()
+        {
+            Console.WriteLine(" Displaying Stored Values in Linked list");
+            Node currentNode = this.head;
+            if (currentNode == null)
+            {
+                Console.WriteLine("Linked List Is Empty");
+            }
+            else
+            {
+
+                while (currentNode != null)
+                {
+                    
+                    Console.WriteLine(currentNode.value);
+                    
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+    }
+
+
+
+
+}
+
---
+++ LinkedList/LinkedList/MainClass.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataStructure
+{
+    internal class mainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            LinkedList.LinkedListClass list = new LinkedList.LinkedListClass();
+            Console.WriteLine("--------------------Welcome to data structure concept------------------------");
+            list.add(56);
+            list.add(30);
+            list.add(70);
+            list.display();
+        }
+    }
+}
+
